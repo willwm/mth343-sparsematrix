1fe52571a6d78c7a231dace94e286e3e0870d0e7
Will Wolff-Myren <willwm@gmail.com>
2018-03-13 21:08:38 -0700

    Expand size of default matrix

--- src/app/mathjs/mathjs.component.ts
+++ src/app/mathjs/mathjs.component.ts
@@ -8,7 +8,8 @@ import * as Plotly from 'plotly.js';
   styleUrls: ['./mathjs.component.css']
 })
 export class MathjsComponent implements OnInit {
-  input = '[[1,2,3,4,5], [0,1,2,3,4], [0,0,1,2,3], [0,0,0,1,2], [0,0,0,0,1]]';
+  // tslint:disable-next-line:max-line-length
+  input = '[[1,2,3,4,5,6,7,8], [0,1,2,3,4,5,6,7], [0,0,1,2,3,4,5,6], [0,0,0,1,2,3,4,5], [0,0,0,0,1,2,3,4], [0,0,0,0,0,1,2,3], [0,0,0,0,0,0,1,2], [0,0,0,0,0,0,0,1]]';
   output: string;
   equation: string;
 
