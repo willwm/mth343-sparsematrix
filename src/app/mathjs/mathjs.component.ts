bf67cef4e00624ec3eb5aa1726d1f937151ca5a8
Will Wolff-Myren <willwm@gmail.com>
2018-03-15 20:46:55 -0700

    Remove monaco-editor

--- src/app/mathjs/mathjs.component.ts
+++ src/app/mathjs/mathjs.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as math from 'mathjs';
 import * as Plotly from 'plotly.js';
-import * as Monaco from 'monaco-editor';
 
 import { LatexService } from '../latex.service';
 
