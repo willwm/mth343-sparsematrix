3a61fe628e541313d907bec9f47abf580c8a1af0
Will Wolff-Myren <willwm@gmail.com>
2018-03-18 21:42:32 -0700

    Create Matrix class (convenience wrapper for mathjs.Matrix for easier TypeScript consumption)

---
+++ src/app/matrix/matrix.ts
@@ -0,0 +1,73 @@
+import * as mathjs from 'mathjs';
+
+type MathArray = number[] | number[][];
+
+/**
+ * Convenience wrapper for mathjs.Matrix for easier TypeScript consumption,
+ * as the @types/mathjs.Matrix typedef does not match the current mathjs library spec.
+ * @export
+ * @class Matrix
+ */
+export class Matrix {
+  private matrix: mathjs.Matrix;
+
+  /**
+   * Creates an instance of Matrix.
+   * @param {(MathArray | mathjs.Matrix)} data
+   * @param {('sparse' | 'dense')} [format]
+   * @memberof Matrix
+   */
+  constructor(
+    private data: MathArray | mathjs.Matrix,
+    private format?: 'sparse' | 'dense',
+    private name?: string
+  ) {
+    this.matrix = math.matrix(data, format);
+  }
+
+  /**
+   * Convenience wrapper for math.matrix.toArray()
+   * @returns {MathArray}
+   * @memberof Matrix
+   */
+  toArray(): MathArray {
+    return (this.matrix as any).toArray();
+  }
+
+  /**
+   * Convenience wrapper for math.matrix.toJSON()
+   * @returns {any} Returns the SparseMatrix version of this Matrix instance, as JSON.
+   * @memberof Matrix
+   */
+  toJSON(): any {
+    const sparse = math.sparse(this.matrix);
+    return sparse.toJSON();
+  }
+
+  isSymmetric(): boolean {
+    const transpose = this.transpose();
+    const result = math.equal(this.matrix, transpose) as boolean;
+    return result;
+  }
+
+  /**
+   * Convenience wrapper for math.multiply() that uses this Matrix instance as the first parameter.
+   * @param {(MathArray | Matrix)} y
+   * @returns {Matrix}
+   * @memberof Matrix
+   */
+  multiplyBy(y: MathArray | Matrix): Matrix {
+    const result = math.multiply(this.matrix, y);
+    return new Matrix(result);
+  }
+
+  /**
+   * Convenience wrapper for math.transpose()
+   * @returns {Matrix}
+   * @memberof Matrix
+   */
+  transpose(): Matrix {
+    const result = math.transpose(this.matrix);
+    return new Matrix(result);
+  }
+}
