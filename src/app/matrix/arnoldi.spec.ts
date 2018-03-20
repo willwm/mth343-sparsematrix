50148ad170378dc13f9f4c6e47a31b7828ce49d0
Will Wolff-Myren <willwm@gmail.com>
2018-03-19 20:03:52 -0700

    Begin constructing unit tests for Arnoldi class

--- src/app/matrix/arnoldi.spec.ts
+++ src/app/matrix/arnoldi.spec.ts
@@ -1,9 +1,22 @@
 import { Arnoldi } from './arnoldi';
 import { Matrix } from './matrix';
 
-describe('Arnoldi', () => {
-  it('should create an instance', () => {
-    const A = new Matrix([[1, 0], [0, 1]], 'A');
-    expect(new Arnoldi(A)).toBeTruthy();
+fdescribe('Arnoldi', () => {
+
+  describe('constructor()', () => {
+    it('should create an instance', () => {
+      const A = new Matrix([[1, 0], [0, 1]], 'A');
+      expect(new Arnoldi(A)).toBeTruthy();
+    });
+
+    it('should create an instance with expected default values', () => {
+      const A = new Matrix([[1, 0], [0, 1]], 'A');
+      const arnoldi = new Arnoldi(A);
+      expect(arnoldi).toBeTruthy();
+      expect(arnoldi.A).toBe(A);
+      expect(arnoldi.m).toEqual(A.rows());
+      expect(arnoldi.n).toEqual(A.rows());
+      expect(arnoldi.v1.toArray()).toEqual([1, 0]);
+    });
   });
 });
--- src/app/matrix/arnoldi.ts
+++ src/app/matrix/arnoldi.ts
@@ -1,11 +1,18 @@
 import { Matrix } from './matrix';
 
 export class Arnoldi {
-  private q: Matrix[];
-  private v: Matrix[];
+  private Q: Matrix[];
+  private V: Matrix[];
   private k: number;
+  readonly n: number;
 
-  constructor(private A: Matrix, private m?: number) {
+  constructor(readonly A: Matrix, readonly m?: number, readonly v1?: Matrix) {
+    this.n = A.rows();
+    m = m || this.n;
+    if (!v1) {
+      v1 = new Matrix([1], 'v1');
+      v1.resize([this.n], 0);
+    }
   }
 
 
