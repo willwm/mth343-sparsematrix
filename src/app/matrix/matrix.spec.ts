e9be236b4451ec4dc6577c58c9be26b8ae2aa881
Will Wolff-Myren <willwm@gmail.com>
2018-03-19 19:19:21 -0700

    Moved defaultArray/defaultName values into each test method to make it more readable

--- src/app/matrix/matrix.spec.ts
+++ src/app/matrix/matrix.spec.ts
@@ -2,39 +2,34 @@ import { Matrix } from './matrix';
 import * as math from 'mathjs';
 
 fdescribe('Matrix', () => {
-  const defaultName = 'A';
-  const defaultArray = [[1, 2], [3, 4]];
-
-  const defaultMatrix = new Matrix([[1, 2], [3, 4]], 'A');
-  const defaultVector = new Matrix([1, 2, 3], 'v');
 
   describe('constructor()', () => {
     it('should create an instance from an Array and string, and set instance properties accordingly', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       expect(matrix).toBeTruthy();
-      expect(matrix.name).toEqual(defaultName);
-      expect(matrix.toArray()).toEqual(defaultArray);
+      expect(matrix.name).toEqual('A');
+      expect(matrix.toArray()).toEqual([[1, 2], [3, 4]]);
     });
 
     it('should create an instance from a string and string, and set instance properties accordingly', () => {
       const json = '[[1, 2], [3, 4]]';
-      const matrix = new Matrix(json, defaultName);
+      const matrix = new Matrix(json, 'A');
       expect(matrix).toBeTruthy();
-      expect(matrix.name).toEqual(defaultName);
-      expect(matrix.toArray()).toEqual(defaultArray);
+      expect(matrix.name).toEqual('A');
+      expect(matrix.toArray()).toEqual([[1, 2], [3, 4]]);
     });
   });
 
   describe('toArray()', () => {
     it('should return expected result from toArray()', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
-      expect(matrix.toArray()).toEqual(defaultArray);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
+      expect(matrix.toArray()).toEqual([[1, 2], [3, 4]]);
     });
   });
 
   describe('toJSON()', () => {
     it('should return expected (sparse) JSON from toJSON()', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const json = matrix.toJSON();
       expect(json.mathjs).toEqual('SparseMatrix');
       expect(json.values).toEqual([1, 3, 2, 4]);
@@ -46,14 +41,14 @@ fdescribe('Matrix', () => {
 
   describe('multiplyBy()', () => {
     it('should correctly multiplyBy() a scalar', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const result = matrix.multiplyBy(2);
       const resultArray = result.toArray();
       expect(resultArray).toEqual([[2, 4], [6, 8]]);
     });
 
     it('should correctly multiplyBy() a vector', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const vector = new Matrix([2, 2], 'v');
       const result = matrix.multiplyBy(vector, 'Av');
       const resultArray = result.toArray();
@@ -61,14 +56,14 @@ fdescribe('Matrix', () => {
     });
 
     it('should correctly multiplyBy() an Array', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const result = matrix.multiplyBy([[2, 0], [0, 2]]);
       const resultArray = result.toArray();
       expect(resultArray).toEqual([[2, 4], [6, 8]]);
     });
 
     it('should correctly multiplyBy() another Matrix instance', () => {
-      const A = new Matrix(defaultArray, defaultName);
+      const A = new Matrix([[1, 2], [3, 4]], 'A');
       const B = new Matrix([[2, 0], [0, 2]], 'B');
       const result = A.multiplyBy(B, 'C');
       expect(result.toArray()).toEqual([[2, 4], [6, 8]]);
@@ -94,18 +89,18 @@ fdescribe('Matrix', () => {
 
   describe('transpose()', () => {
     it('should return the matrix transpose from transpose(), and should not modify the original', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const transpose = matrix.transpose();
       expect(transpose.toArray()).toEqual([[1, 3], [2, 4]]);
-      expect(matrix.toArray()).toEqual(defaultArray);
+      expect(matrix.toArray()).toEqual([[1, 2], [3, 4]]);
     });
 
     it('for a matrix A, (A^T)T should equal A', () => {
-      const matrix = new Matrix(defaultArray, defaultName);
+      const matrix = new Matrix([[1, 2], [3, 4]], 'A');
       const transpose = matrix.transpose();
       expect(transpose.toArray()).toEqual([[1, 3], [2, 4]]);
       const involution = transpose.transpose();
-      expect(involution.toArray()).toEqual(defaultArray);
+      expect(involution.toArray()).toEqual([[1, 2], [3, 4]]);
     });
   });
 
@@ -131,7 +126,7 @@ fdescribe('Matrix', () => {
     });
 
     it('should return the correct size() for a matrix', () => {
-      const A = new Matrix(defaultArray, defaultName);
+      const A = new Matrix([[1, 2], [3, 4]], 'A');
       const size = A.size();
       expect(size).toEqual([2, 2]);
     });
@@ -147,7 +142,7 @@ fdescribe('Matrix', () => {
     });
 
     it('should correctly determine if the Matrix instance is *not* a vector', () => {
-      const A = new Matrix(defaultArray, defaultName);
+      const A = new Matrix([[1, 2], [3, 4]], 'A');
       expect(A.isVector()).toEqual(false);
     });
   });
@@ -169,4 +164,5 @@ fdescribe('Matrix', () => {
       expect(vT.rows()).toEqual(1);
     });
   });
+
 });
