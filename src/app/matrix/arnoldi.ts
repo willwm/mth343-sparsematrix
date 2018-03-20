dcb663a41fff3ff99f6229eec0815c3c7b824db5
Will Wolff-Myren <willwm@gmail.com>
2018-03-19 20:12:37 -0700

    readonly modifiers didn't work like I expected in the constructor =P

--- src/app/matrix/arnoldi.ts
+++ src/app/matrix/arnoldi.ts
@@ -5,13 +5,15 @@ export class Arnoldi {
   private V: Matrix[];
   private k: number;
   readonly n: number;
+  readonly m: number;
+  readonly v1: Matrix;
 
-  constructor(readonly A: Matrix, readonly m?: number, readonly v1?: Matrix) {
+  constructor(readonly A: Matrix, _m?: number, _v1?: Matrix) {
     this.n = A.rows();
-    m = m || this.n;
-    if (!v1) {
-      v1 = new Matrix([1], 'v1');
-      v1.resize([this.n], 0);
+    this.m = _m || this.n;
+    if (!_v1) {
+      this.v1 = new Matrix([1], 'v1');
+      this.v1.resize([this.n], 0);
     }
   }
 
