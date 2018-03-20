edceba302bf5b99b6f3a43cd4ebe000099f7d918
Will Wolff-Myren <willwm@gmail.com>
2018-03-19 22:05:41 -0700

    Implement q2()

--- src/app/matrix/arnoldi.spec.ts
+++ src/app/matrix/arnoldi.spec.ts
@@ -1,7 +1,7 @@
 import { Arnoldi } from './arnoldi';
 import { Matrix } from './matrix';
 
-fdescribe('Arnoldi', () => {
+describe('Arnoldi', () => {
 
   describe('constructor()', () => {
     it('should create an instance', () => {
@@ -39,6 +39,22 @@ fdescribe('Arnoldi', () => {
       const arnoldi = new Arnoldi(A);
       const q1 = arnoldi.q1(v1);
       expect(q1.toArray()).toEqual([1, 0, 0]);
+    });
+  });
+
+  describe('q2', () => {
+    it('should return a normalized v2 - q1t*v2*q1', () => {
+      const A = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 'A');
+      const q1 = new Matrix([1, 0, 0], 'q1');
+      const v1 = new Matrix([1, 0, 0], 'v1');
+      const arnoldi = new Arnoldi(A);
+
+      const Aq1 = A.multiplyBy(q1, 'Aq1');
+      const v2 = arnoldi.vk(A, q1, 2);
+      expect(v2.toArray()).toEqual(Aq1.toArray());
+
+      const q2 = arnoldi.q2(q1, v2);
+      console.log(q2);
     });
   });
 
--- src/app/matrix/arnoldi.ts
+++ src/app/matrix/arnoldi.ts
@@ -17,12 +17,20 @@ export class Arnoldi {
     }
   }
 
-  q1(v1: Matrix) {
+  q1(v1: Matrix): Matrix {
     const q1 = v1.normalize('q1');
     return q1;
   }
 
-  vk(A: Matrix, qPrev: Matrix, k: number) {
+  q2(q1: Matrix, v2: Matrix) {
+    const qtv2 = q1.multiplyBy(v2); // Should be a scalar...
+    const qtv2q = q1.multiplyBy(qtv2); // Since qtv2 is a scalar, call the multiplyBy on q1 instead.
+    const q2DeNorm = v2.subtractBy(qtv2q);
+    const q2 = q2DeNorm.normalize('q2');
+    return q2;
+  }
+
+  vk(A: Matrix, qPrev: Matrix, k: number): Matrix {
     const vk = A.multiplyBy(qPrev, `v${k}`);
     return vk;
   }
