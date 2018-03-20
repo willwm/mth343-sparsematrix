5640272077364e988e6e988c1fa8c6d2007d4c2d
Will Wolff-Myren <willwm@gmail.com>
2018-03-19 20:38:06 -0700

    Implement q1() and vk() methods

--- src/app/matrix/arnoldi.spec.ts
+++ src/app/matrix/arnoldi.spec.ts
@@ -18,5 +18,40 @@ fdescribe('Arnoldi', () => {
       expect(arnoldi.n).toEqual(A.rows());
       expect(arnoldi.v1.toArray()).toEqual([1, 0]);
     });
+
+    it('should let an m < n be assigned', () => {
+      const A = new Matrix([[1, 2, 3], [4, 5, 6]], 'A');
+      const arnoldi = new Arnoldi(A, 2);
+      expect(arnoldi.m).toEqual(2);
+    });
+
+    it('should *not* let an m > n be assigned', () => {
+      const A = new Matrix([[1, 2, 3], [4, 5, 6]], 'A');
+      const arnoldi = new Arnoldi(A, 3);
+      expect(arnoldi.m).toEqual(A.rows());
+    });
+  });
+
+  describe('q1()', () => {
+    it('should return a normalized v1', () => {
+      const A = new Matrix([[1, 2, 3], [4, 5, 6]], 'A');
+      const v1 = new Matrix([1, 0, 0], 'v1');
+      const arnoldi = new Arnoldi(A);
+      const q1 = arnoldi.q1(v1);
+      expect(q1.toArray()).toEqual([1, 0, 0]);
+    });
+  });
+
+  describe('vk()', () => {
+    it('should return A q_{k-1}', () => {
+      const A = new Matrix([[1, 2, 3], [4, 5, 6]], 'A');
+      const q1 = new Matrix([1, 0, 0], 'q1');
+      const Aq1 = A.multiplyBy(q1, 'Aq1');
+      const arnoldi = new Arnoldi(A);
+
+      const k = 2;
+      const vk = arnoldi.vk(A, q1, k);
+      expect(vk.toArray()).toEqual(Aq1.toArray());
+    });
   });
 });
--- src/app/matrix/arnoldi.ts
+++ src/app/matrix/arnoldi.ts
@@ -1,21 +1,30 @@
 import { Matrix } from './matrix';
 
 export class Arnoldi {
-  private Q: Matrix[];
-  private V: Matrix[];
-  private k: number;
+  private q: Array<Matrix>;
+  private v: Array<Matrix>;
+  private k = 0;
   readonly n: number;
   readonly m: number;
   readonly v1: Matrix;
 
   constructor(readonly A: Matrix, _m?: number, _v1?: Matrix) {
     this.n = A.rows();
-    this.m = _m || this.n;
+    this.m = (_m && _m < this.n) ? _m : this.n;
     if (!_v1) {
       this.v1 = new Matrix([1], 'v1');
       this.v1.resize([this.n], 0);
     }
   }
 
+  q1(v1: Matrix) {
+    const q1 = v1.normalize('q1');
+    return q1;
+  }
+
+  vk(A: Matrix, qPrev: Matrix, k: number) {
+    const vk = A.multiplyBy(qPrev, `v${k}`);
+    return vk;
+  }
 
 }
